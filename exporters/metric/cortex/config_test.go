b7bb9b327b73e4be74c7778596774ab0ff8b8207
Eric Lee <elleea@amazon.com>
2020-08-04 09:08:15 -0700

    Add helper function for creating YAML files and add TestNewConfig

--- exporters/metric/cortex/config_test.go
+++ exporters/metric/cortex/config_test.go
@@ -1,5 +1,15 @@
 package cortex
 
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
 // YAML file that produces a Config struct without errors.
 var validYAML = []byte(`url: /api/prom/push
 remote_timeout: 30s
@@ -33,7 +43,7 @@ tls_config:
 
 // YAML file with both password and password_file properties. It should fail to produce a Config
 // struct since password and password_file are mutually exclusive.
-var noURLYAML = []byte(`remote_timeout: 30s
+var noEndpointYAML = []byte(`remote_timeout: 30s
 name: Valid Config Example
 basic_auth:
   username: user
@@ -102,4 +112,61 @@ var validConfig = Config{
 	},
 	ProxyURL: "",
 	Client:   nil,
+}
+
+// initYAML creates a YAML file at a given filepath. It does not remove any created directories or
+// files.
+func initYAML(yamlBytes []byte, path string) error {
+	dirPath := filepath.Dir(path)
+
+	err := os.MkdirAll(dirPath, 0755)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path, yamlBytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TestNewConfig tests whether NewConfig returns a correct Config struct. It checks whether the YAML
+// file was read correctly and whether validation of the struct succeeded.
+func TestNewConfig(t *testing.T) {
+	tests := []struct {
+		name           string
+		yamlFile       []byte
+		fileName       string
+		expectedConfig *Config
+		expectedError  error
+	}{
+		{
+			"Valid Config file",
+			validYAML,
+			"config.yml",
+			&validConfig,
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Create YAML file in the current directory.
+			if err := initYAML(test.yamlFile, test.fileName); err != nil {
+				t.Fatalf("Failed to initialize YAML file with error %v", err)
+			}
+			defer os.RemoveAll(test.fileName)
+
+			// Create new Config struct and verify errors and contents.
+			config, err := NewConfig(test.fileName)
+			if !errors.Is(err, test.expectedError) {
+				t.Fatalf("Received error %v, wanted %v", err, test.expectedError)
+			}
+			if !cmp.Equal(config, test.expectedConfig) {
+				t.Fatalf("Received Config %v, wanted %v", config, test.expectedConfig)
+			}
+		})
+	}
 }
