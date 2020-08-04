15bf1df5e304f2db3c8e426148b8988427f3f8fb
Eric Lee <elleea@amazon.com>
2020-08-04 09:10:59 -0700

    Add NewConfig function and Options interface,

--- exporters/metric/cortex/config.go
+++ exporters/metric/cortex/config.go
@@ -1,6 +1,10 @@
 package cortex
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/spf13/viper"
+)
 
 // Config contains properties the Exporter uses to export metrics data to Cortex.
 type Config struct {
@@ -13,4 +17,31 @@ type Config struct {
 	TLSConfig       map[string]string `mapstructure:"tls_config"`
 	ProxyURL        string            `mapstructure:"proxy_url"`
 	Client          *http.Client
+}
+
+// Option sets an option for a Config struct.
+type Option interface {
+	Apply(*Config)
+}
+
+// NewConfig creates a Config struct with a YAML file and applies Option functions to the Config
+// struct.
+func NewConfig(filename string, opts ...Option) (*Config, error) {
+	var config Config
+	for _, opt := range opts {
+		opt.Apply(&config)
+	}
+
+	viper.SetConfigName(filename)
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(filename)
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+	return &config, nil
 }
