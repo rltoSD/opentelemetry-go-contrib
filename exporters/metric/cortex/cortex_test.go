5882937ff1c6907e711ba2f483e295097a727a49
Eric Lee <elleea@amazon.com>
2020-08-04 22:46:51 -0700

    Add NewRawExporter and TestNewRawExporter

--- exporters/metric/cortex/cortex_test.go
+++ exporters/metric/cortex/cortex_test.go
@@ -3,6 +3,7 @@ package cortex_test
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"go.opentelemetry.io/otel/sdk/export/metric"
 	"go.opentelemetry.io/otel/sdk/export/metric/aggregation"
 	"opentelemetry.io/contrib/exporters/metric/cortex"
@@ -15,5 +16,18 @@ func TestExportKindFor(t *testing.T) {
 
 	if got != want {
 		t.Errorf("ExportKindFor() =  %q, want %q", got, want)
+	}
+}
+
+// TestNewRawExporter tests whether NewRawExporter successfully creates an Exporter with the same
+// Config struct as the one passed in.
+func TestNewRawExporter(t *testing.T) {
+	exporter, err := cortex.NewRawExporter(ValidConfig)
+	if err != nil {
+		t.Errorf("Failed to create exporter with error %v", err)
+	}
+
+	if !cmp.Equal(ValidConfig, exporter.Config) {
+		t.Errorf("Got configuration %v, wanted %v", exporter.Config, ValidConfig)
 	}
 }
