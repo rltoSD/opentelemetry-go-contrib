54a00c289790251daa9e0c2b994af7ad6735bf33
Eric Lee <elleea@amazon.com>
2020-08-12 23:55:18 -0700

    Implement TestSecureTransport that verifies the Authorization header

--- exporters/metric/cortex/client_test.go
+++ exporters/metric/cortex/client_test.go
@@ -14,10 +14,44 @@
 package cortex
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // TestSecureTransport checks whether http requests sent using a SecureTransport has the
 // correct Authorization header added.
 func TestSecureTransport(t *testing.T) {
+	tests := []struct {
+		testName                string
+		expectedAuthHeaderValue string
+		expectedError           error
+	}{}
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			// Set up a test server that runs a handler function when it receives a http
+			// request. The server writes the request's Authorization header to the
+			// response body.
+			handler := func(rw http.ResponseWriter, req *http.Request) {
+				authHeaderValue := req.Header.Get("Authorization")
+				rw.Write([]byte(authHeaderValue))
+			}
+			server := httptest.NewServer(http.HandlerFunc(handler))
+			defer server.Close()
+
+			// Create a SecureTransport that adds an Authorization header.
+			server.Client().Transport = &SecureTransport{
+				rt: http.DefaultTransport,
+			}
+
+			// Verify that the Transport successfully added the Authorization header.
+			resp, _ := server.Client().Get(server.URL)
+			body, _ := ioutil.ReadAll(resp.Body)
+			authHeaderValue := string(body)
+			require.Equal(t, authHeaderValue, test.expectedAuthHeaderValue)
+		})
+	}
 }
